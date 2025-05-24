d281a2af40f1f57fdb334f7eb420830af9b74d7b
saravuth-pi <pulanet@hotmail.com>
2025-05-25 00:38:31 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -116,7 +116,7 @@ export default function Home() {
           </div>
   
           <h3>รายงานเหตุแผ่นดินไหวในภูมิภาค</h3>
-          <LatestQuakes usgsQuakes={Map latest} tmdQuakes={tmdQuakes} />
+          <LatestQuakes usgsQuakes={usgsQuakes} tmdQuakes={tmdQuakes} />
         </div>
       </div>
 
