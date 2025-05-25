4fd9a15846d541688d29044cf0c6efa176582c97
saravuth-pi <pulanet@hotmail.com>
2025-05-25 15:27:29 +0700

    Update LatestQuakes.js

--- components/LatestQuakes.js
+++ components/LatestQuakes.js
@@ -43,8 +43,7 @@ export default function LatestQuakes({ usgsQuakes = [], tmdQuakes = [] }) {
     const distance = haversine(PAT1_LAT, PAT1_LNG, q.lat, q.lon);
     const age = now - quakeTime.getTime();
     const timeInBangkok = new Date(new Date(q.timestamp).getTime() + 7 * 60 * 60 * 1000);
-    //
-    console.log(timeInBangkok);
+    // console.log(timeInBangkok);
     if (distance <= 3000 && q.mag >= 2 && age <= 24 * 3600 * 1000) {
       quakes.push({
         source: 'TMD',
