97a56ac0889d1c32b32d524cf6a1ad88f93fdaca
saravuth-pi <pulanet@hotmail.com>
2025-06-03 01:03:31 +0700

    eer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "62396c9c4e1a4e14"; }
+/******/ 		__webpack_require__.h = function() { return "58ccc77688454c27"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
