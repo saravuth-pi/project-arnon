191b355192a062a3a3942284e2fca4195ee3fbcd
saravuth-pi <pulanet@hotmail.com>
2025-06-10 17:40:01 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -117,10 +117,6 @@ export default function Home() {
         <LatestQuakes tmdQuakes={tmdQuakes} usgsQuakes={usgsQuakes} />
       </div>
 
-      {/* MAP */}
-      <div style={{ height: '40vh', width: '100vw', marginBottom: 3 }}>
-        <Map latest={allDataPoints} />
-      </div>
 
       {/* AQI Panel <AQIPanel /> */}
     </div>
