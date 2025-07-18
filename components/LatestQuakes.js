b45a73f5160be6bd2f35b8b1cb52145287bc5116
Saravuth Pinmuk <pulanet@hotmail.com>
2025-07-18 10:10:52 +0700

    สีพิน 5

--- components/LatestQuakes.js
+++ components/LatestQuakes.js
@@ -172,8 +172,8 @@ export default function LatestQuakes({ tmdQuakes = [] }) {
 }
 
 function getColor(mag) {
-  const normalizedMag = Math.min(Math.max(mag, 2.5), 9.0)
-  const fraction = (normalizedMag - 2.5) / 6.0
+  const normalizedMag = Math.min(Math.max(mag, 1.5), 8.0)
+  const fraction = (normalizedMag - 1.5) / 5.0
   const hue = (1 - fraction) * 120
   return `hsl(${hue}, 100%, 50%)`
 }
